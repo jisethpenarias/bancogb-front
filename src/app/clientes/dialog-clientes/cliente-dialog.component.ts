4fbc984bbe7ae5f1e13659e885430933e4751481
jpenariasholberton <1875@holbertonschool.com>
2021-05-25 22:39:24 -0500

    Drop Down Tipos de Documentos formulario creacion clientes

--- src/app/clientes/dialog-clientes/cliente-dialog.component.ts
+++ src/app/clientes/dialog-clientes/cliente-dialog.component.ts
@@ -2,7 +2,10 @@ import { Component, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Cliente } from "../admin-clientes/admin-clientes.component";
 
-
+export interface TipoDocumento {
+  value: string,
+  viewValue: string
+}
 
 @Component({
   selector: 'cliente-dialog',
@@ -10,7 +13,11 @@ import { Cliente } from "../admin-clientes/admin-clientes.component";
   styleUrls: ['cliente-dialog.component.css']
 })
 export class ClienteDialog {
-
+  tiposDocumentos: TipoDocumento[] = [
+    {value: "CC", viewValue: "Cedula de Ciudadania"},
+    {value: "TI", viewValue: "Tarjeta de Identidad"},
+    {value: "CE", viewValue: "Cedula de Extranjeria"},
+  ]
   constructor(
     public dialogRef: MatDialogRef<ClienteDialog>,
     @Inject(MAT_DIALOG_DATA) public data: Cliente) {}
